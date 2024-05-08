764bb05368c42d2dad5f9e8602677e8667926902
nassahel <nassahel.elias@gmail.com>
2024-05-08 20:06:11 -0300

    se cambio la fuente del nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ import CartIcon from './CartIcon';
 const Navbar = () => {
   return (
     <div className='h-[4rem] hidden shadow-md text-white gap-2 bg-orange-500 lg:flex sticky top-0 z-30 items-center justify-between pl-4 pr-3'>
-      <Link to="/" className='font-bold text-2xl xl:w-1/5'>
+      <Link to="/" className='text-2xl xl:w-1/5'>
         <img src={LogoTransparente} alt="" className='h-16' />
       </Link>
       <div className='flex justify-center flex-grow max-w-[60rem] '>
         <Searcher type='1' />
       </div>
-      <div className='flex  gap-3 font-semibold items-center justify-end'>
+      <div className='flex  gap-3 items-center justify-end'>
         <Link className='border-b-2 border-transparent hover:border-white duration-300' to="/">Inicio</Link>
         <Link className='border-b-2 border-transparent hover:border-white duration-300' to="about">Nosotros</Link>
         <Link onClick={() => alert("Ups! 😥 Página en desarrollo. \nEsta funcion estara disponible proximamente!")} className='border-b-2 border-transparent hover:border-white duration-300' to="/admin/*">Administración</Link>
